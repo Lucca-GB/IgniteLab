7ebb44dc532166decd01e017f632f418e77b538f
Lucca <luccag.barreto@gmail.com>
2022-06-21 21:39:24 -0300

    estilização sendo feita, primeiro component criado

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { client } from './lib/apollo'
 
+
+// query / mutation
+
+// query = buscar dados (get)
+// mutatio = criar, alterar e deletar dados
+
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
