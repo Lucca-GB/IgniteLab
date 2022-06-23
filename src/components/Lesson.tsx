f8083fd7670bbc2d87882600b0546fb13e668b8b
Lucca <luccag.barreto@gmail.com>
2022-06-23 12:43:38 -0300

    consumindo lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -4,6 +4,7 @@ import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format} from 'date-fns'
 //além de nos permitir escolher a lingua
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
      title: string;
@@ -25,12 +26,12 @@ export function Lesson(props: LessonProps) {
      })
 
      return (
-          <a href="#">
+          <Link to={`/event/lesson/${props.slug}`} className="group">
                <span className="text-gray-300">
                     {availableDateFormatted}
                </span>
 
-               <div className="rounded border border-gray-500 p-4 mt-2">
+               <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
                     <header className="flex items-center justify-between">
 
 
@@ -56,6 +57,6 @@ export function Lesson(props: LessonProps) {
                          {props.title}
                     </strong>
                </div>
-          </a>
+          </Link>
      )
 }
